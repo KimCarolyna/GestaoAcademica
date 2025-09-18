a4a5b5dca48988154e055c7c08ba3d5a6e7027fe
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-18 17:26:32 -0300

    atualização e renomeação
    Fiz um código rápido e renomeei o nome do arquivo que estava errado. Esse arquivo ainda está sujeito a atualizações

---
+++ dao/Alunodao.java
@@ -0,0 +1,14 @@
+public classs Alunodao {
+  public void salvar(Aluno aluno){
+    System.out.println("Aluno "  + aluno.etNome() + " salvo no banco de dados.");
+  }
+
+  public Aluno buscarPorId(int id){
+    System.out.println("Buscando aluno com ID " + id);
+  try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+    }
+  }
+}
