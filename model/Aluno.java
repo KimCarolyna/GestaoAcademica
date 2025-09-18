836f1d694d67e87f49f1a6738bc4ef9e7e18a83c
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-18 17:18:47 -0300

    Atualização Aluno.java
    Terminado o código. Fiz algumas correções e terminei o que não tinha conseguido terminar antes. sujeito a correções futuras

--- model/Aluno.java
+++ model/Aluno.java
@@ -9,9 +9,25 @@ public Aluno(int id, String nome, String email){
   this.email = email;
   }
   public int getID(){
-    return id
+    return id;
   }
   public void setID(int id){
     this.id = id;
+  }
+
+  public String getNome(){
+    return nome;
+  }
+
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public void setEmail(String email){
+    this.email = email
   }
 }
