68526ea8aab183ae06478484165cb9744c8a21fe
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-11-04 13:29:01 -0300

    Deletando comentários
    comentários criados pelo mysql que eu estou removendo

--- Banco de dados/gestaoacademica_disciplina.sql
+++ Banco de dados/gestaoacademica_disciplina.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
---
--- Host: localhost    Database: gestaoacademica
--- ------------------------------------------------------
--- Server version	8.0.43
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `disciplina`
---
-
-DROP TABLE IF EXISTS `disciplina`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `disciplina` (
   `id_disciplina` int NOT NULL AUTO_INCREMENT,
   `codigo` varchar(10) NOT NULL,
@@ -34,25 +10,5 @@ CREATE TABLE `disciplina` (
   KEY `id_professor` (`id_professor`),
   CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`id_professor`) REFERENCES `professor` (`id_professor`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `disciplina`
---
-
-LOCK TABLES `disciplina` WRITE;
-/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
 INSERT INTO `disciplina` VALUES (1,'ENG001','Engenharia de Software',80,'3º Semestre',1),(2,'MAT101','Cálculo I',60,'1º Semestre',2),(3,'FIS102','Física Geral',60,'1º Semestre',3),(4,'ELE103','Minecraft',60,'2º Semestre',4),(5,'ADM104','Gestão de Projetos',60,'4º Semestre',5),(6,'CIV105','Mecânica',80,'5º Semestre',6),(7,'COMP106','Inteligência Artificial',80,'6º Semestre',7),(8,'EDU107','Como ligar tomadas',60,'2º Semestre',8),(9,'QUI108','Química Orgânica',60,'1º Semestre',9),(10,'BIO109','Desenvolvimento de Jogos',60,'2º Semestre',10);
-/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-10-20 21:51:27
